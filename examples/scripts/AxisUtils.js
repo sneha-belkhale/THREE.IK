92c6268c91fb73ba99156b4edac3fb1d0f1270e4
Sneha Belkhale <sbelkhale@yahoo.com>
2019-03-10 00:47:21 +0700

    adding comments

--- examples/scripts/AxisUtils.js
+++ examples/scripts/AxisUtils.js
@@ -4,6 +4,55 @@ const t2 = new THREE.Vector3();
 const t3 = new THREE.Vector3();
 const m1 = new THREE.Matrix4();
 
+/**
+ * Takes in a rootBone and recursively traverses the bone heirarchy,
+ * setting each bone's +Z axis to face it's child bones. The IK system follows this
+ * convention, so this step is necessary to update the bindings of a skinned mesh. 
+ *
+ * @param {THREE.BONE} rootBone
+ * @param {THREE.Scene} scene
+ */
+
+function setZForward(rootBone, scene) {
+  var worldPos = {}
+  getOriginalWorldPositions(rootBone, worldPos)
+  updateTransformations(rootBone, worldPos, scene);
+}
+
+function updateTransformations(parentBone, worldPos, scene) {
+    var averagedDir = new THREE.Vector3();
+    parentBone.children.forEach((childBone) => {
+      //average the child bone world pos
+      var childBonePosWorld = worldPos[childBone.id][0];
+      averagedDir.add(childBonePosWorld);
+    });
+
+    averagedDir.multiplyScalar(1/(parentBone.children.length));
+
+    //set quaternion
+    parentBone.quaternion.copy(RESETQUAT);
+    parentBone.updateMatrixWorld();
+
+    //get the child bone position in local coordinates
+    var childBoneDir = parentBone.worldToLocal(averagedDir.clone()).normalize();
+
+    //preserve the original up vectors
+    var up = _worldToLocalDirection(worldPos[parentBone.id][1].clone(), parentBone)
+    setQuaternionFromDirection(childBoneDir, up, parentBone.quaternion)
+    parentBone.updateMatrixWorld();
+
+    //set child bone position relative to the new parent matrix.
+    parentBone.children.forEach((childBone) => {
+      var childBonePosWorld = worldPos[childBone.id][0].clone();
+      parentBone.worldToLocal(childBonePosWorld);
+      childBone.position.copy(childBonePosWorld);
+    });
+
+    parentBone.children.forEach((childBone) => {
+      updateTransformations(childBone, worldPos, scene);
+    })
+}
+
 //borrowing this from utils.js , not sure how to import it
 function setQuaternionFromDirection(direction, up, target) {
   const x = t1;
@@ -55,44 +104,4 @@ function _localToWorldDirection(direction, sparent) {
   const parent = sparent.matrixWorld;
   direction.transformDirection(parent);
   return direction;
-}
-
-function setZForward(rootBone, scene) {
-  var worldPos = {}
-  getOriginalWorldPositions(rootBone, worldPos)
-  updateTransformations(rootBone, worldPos, scene);
-}
-
-function updateTransformations(parentBone, worldPos, scene) {
-    var averagedDir = new THREE.Vector3();
-    parentBone.children.forEach((childBone) => {
-      //average the child bone world pos
-      var childBonePosWorld = worldPos[childBone.id][0];
-      averagedDir.add(childBonePosWorld);
-    });
-
-    averagedDir.multiplyScalar(1/(parentBone.children.length));
-
-    //set quaternion
-    parentBone.quaternion.copy(RESETQUAT);
-    parentBone.updateMatrixWorld();
-
-    //get the child bone position in local coordinates
-    var childBoneDir = parentBone.worldToLocal(averagedDir.clone()).normalize();
-
-    //preserve the original up vectors
-    var up = _worldToLocalDirection(worldPos[parentBone.id][1].clone(), parentBone)
-    setQuaternionFromDirection(childBoneDir, up, parentBone.quaternion)
-    parentBone.updateMatrixWorld();
-
-    //set child bone position relative to the new parent matrix.
-    parentBone.children.forEach((childBone) => {
-      var childBonePosWorld = worldPos[childBone.id][0].clone();
-      parentBone.worldToLocal(childBonePosWorld);
-      childBone.position.copy(childBonePosWorld);
-    });
-
-    parentBone.children.forEach((childBone) => {
-      updateTransformations(childBone, worldPos, scene);
-    })
 }
--- src/utils.js
+++ src/utils.js
@@ -9,6 +9,9 @@ const t1 = new Vector3();
 const t2 = new Vector3();
 const t3 = new Vector3();
 const m1 = new Matrix4();
+const t = new Vector3();
+const q = new Quaternion();
+const p = new Plane();
 
 /**
  * Returns the world position of object and sets
@@ -119,10 +122,14 @@ export function transformPoint(vector, matrix, target) {
   target.set(x / w, y / w, z / w);
 };
 
-const t = new Vector3();
-const q = new Quaternion();
-const p = new Plane();
-
+/**
+ * Aligns two vectors about their cross product. Returns a quaternion that will
+ * map the fromDir vector to the toDir vector.
+ *
+ * @param {THREE.Vector3} fromDir
+ * @param {THREE.Vector3} toDir
+ * @return {THREE.Quaternion | null}
+ */
 export function getAlignmentQuaternion(fromDir, toDir) {
   const adjustAxis = t.crossVectors(fromDir, toDir).normalize();
   const adjustAngle = fromDir.angleTo(toDir);
@@ -134,13 +141,30 @@ export function getAlignmentQuaternion(fromDir, toDir) {
   return null;
 }
 
-export function getAlignmentQuaternionOnPlane(toVector, fromVector, normal) {
+/**
+ * Aligns two vectors, but ensuring that the result is on a plane oriented by the normal param.
+ * Returns a quaternion that will map the fromDir vector to the toDir vector.
+ *
+ * @param {THREE.Vector3} fromDir
+ * @param {THREE.Vector3} toDir
+ * @param {THREE.Vector3} normal
+ * @return {THREE.Vector3 | normal}
+ */
+export function getAlignmentQuaternionOnPlane(toDir, fromDir, normal) {
   p.normal = normal;
-  const projectedVec = p.projectPoint(toVector, new Vector3()).normalize();
-  const quat = getAlignmentQuaternion(fromVector, projectedVec);
+  const projectedVec = p.projectPoint(toDir, new Vector3()).normalize();
+  const quat = getAlignmentQuaternion(fromDir, projectedVec);
   return quat;
 }
 
+/**
+ * Takes a three.js bone and rotates it on a (hinge) axis to the final
+ * direction.
+ *
+ * @param {THREE.BONE} bone
+ * @param {THREE.Vector3} direction
+ * @param {THREE.Vector3} axis
+ */
 export function rotateOnAxis(bone, direction, axis) {
   var forward = new Vector3(0,0,1).applyQuaternion(bone.quaternion);
   var q = getAlignmentQuaternionOnPlane(direction,forward,axis);
